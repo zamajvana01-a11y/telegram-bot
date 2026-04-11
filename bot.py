16e11e3f75a1f8e279e865d015924391b6bb90cc
zamajvana01-a11y <zamajvana01@gmail.com>
2026-04-11 20:04:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-TOKEN = "TOKEN = "7139683001:AAFpIMrMSENgZwiKLOpYlROymbXJFOjO5oQ""
+TOKEN = "7139683001:AAFpIMrMSENgZwiKLOpYlROymbXJFOjO5oQ"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
